ab184307ebd78489cec9d094e024f25fc65606f5
David Brill <6david6brill6@gmail.com>
2024-08-11 22:06:30 -0400

    Update msan compiler flags used for building gtest

--- build-gtest.sh
+++ build-gtest.sh
@@ -1,8 +1,8 @@
-MSAN_CFLAGS="-fsanitize=memory -stdlib=libc++ -L/usr/lib -lc++abi"
+MSAN_CFLAGS="fsanitize=memory -fsanitize-memory-track-origins -fPIE -pie -fno-omit-frame-pointer -nostdinc++ -nostdlib++ -isystem /usr/include/libcxx-msan/c++/v1 -L /usr/lib/libcxx-msan -Wl,-rpath,/usr/lib/libcxx-msan -lc++"
 
 git clone https://github.com/google/googletest.git --branch main --depth 1
 pushd googletest
-mkdir build
+mkdir -p build
 cmake -B build \
     -DCMAKE_BUILD_TYPE=Release \
     -G "Unix Makefiles" \
